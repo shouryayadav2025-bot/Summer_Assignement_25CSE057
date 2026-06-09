5440bcaa3f388bbbcee7501b1b00273b58a32116
shourya yadav <shourya.yadav2025@glbajajgroup.org>
2026-06-09 21:03:32 +0530

    Day 1 Uploaded

---
+++ Day 1/Q1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    int sum;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    sum = n * (n + 1)/2;
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+
+    return 0;
+}
